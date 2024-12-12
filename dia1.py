f70b99ea6d892679fc1ecf1bca0634dbad11635f
Felipe Oliveira <felipe.j.o.86@gmail.com>
2024-12-12 08:54:04 -0300

    first commit

---
+++ dia1.py
@@ -0,0 +1,3 @@
+print("Hello Python")
+print("É sempre bom estar por aqui!")
+print("Seja bem-vindo Felipe")
---
+++ dia2.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+idade = 38
+altura = 1.80
+estudante = True
+
+
+print(nome)
+print(idade)
+print(altura)
+print(estudante)
+
+ano_nasc = 2024 - idade
+print(ano_nasc)
