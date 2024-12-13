fa6f72a28705d725e083013afd10459336753dc7
Felipe Oliveira <felipe.j.o.86@gmail.com>
2024-12-13 09:27:07 -0300

    complemento aula dia 2

--- dia2.py
+++ dia2.py
@@ -9,5 +9,66 @@ print(idade)
 print(altura)
 print(estudante)
 
+ano_nascimento = 2024 - idade
+print("Ano de nascimento: ", ano_nascimento)
+
 ano_nasc = 2024 - idade
 print(ano_nasc)
+
+
+data_nasc = '28/12/2002'
+print(data_nasc)
+print(type(data_nasc))
+
+# operadores
+print(5 + 1.2)
+print(10 * 2)
+print(5 / 10)
+print(50 - 25)
+
+nomeUsuario = "Felipe"
+
+frase = "Olá " + nome + " tudo bem?"
+print(frase)
+
+# comparações
+
+maior = 15 > 2 
+print(maior)
+
+menor = 5 < 2
+
+print(menor)
+
+print(5 == 5)
+print("teste" == "teste")
+print("Felipe" == "felipe")
+print(15 != 2)
+
+# Revisão
+
+num = 10
+num2 = num
+print(num2)
+num = 12
+print(num)
+
+print("Numero: ", num)
+operacao_matematica = num + 12
+print(operacao_matematica)
+
+comp = 12 > 5
+print(comp)
+
+print((12 +1)*2)
+
+
+num3 = 10
+num4 = num + num3
+print(num4)
+
+print(15.2 % 5)
+print(15.2 / 5)
+print(-3 + 12)
+print(2**2)
+print(2**4)
